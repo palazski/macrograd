f4ef2b24868c10e1b26a6713d39507babdce4bb5
palazski <palazski@gmail.com>
2024-06-22 22:25:05 +0300

    feat(setup): rename setup.py variables

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="micrograd",
-    version="0.1.0",
-    author="Andrej Karpathy",
-    author_email="andrej.karpathy@gmail.com",
-    description="A tiny scalar-valued autograd engine with a small PyTorch-like neural network library on top.",
+    name="macrograd",
+    version="0.0.1",
+    author="Abdullah Palaz",
+    author_email="palazski@gmail.com",
+    description="A tiny scalar-valued autograd engine with PyTorch-like neural network library on top, based on Andrej Karpathy's micrograd",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/karpathy/micrograd",
+    url="https://github.com/palazski/macrograd",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
