2f6e2ec5120c7f050abb57bb052feaff3e019caf
Abdullah Palaz <56722644+palazski@users.noreply.github.com>
2024-06-30 15:05:19 +0000

    feat(Tensor): better initialization, finished slicing, better shape

--- macrograd/engine/tensor.py
+++ macrograd/engine/tensor.py
@@ -5,128 +5,62 @@ class Tensor:
 
     # TODO: Maybe pass shape instead of value?
     def __init__(self, data=None) -> None:
-        self.data = data
-
-        self.shape = None
-        if data:
-            self._set_shape()
+        self._set_value(data)
 
         self.grad = None
         self.backward = None
 
     def _set_shape(self, shape=None):
         if shape:
-            assert isinstance(shape, tuple, list, int), 'shape must be of type tuple, list, or int'
+            assert isinstance(shape, (tuple, list, int)), 'shape must be of type tuple, list, or int'
             self.shape = shape
             return shape
-
-        # TODO: Make this better
-        def get_shape(lst):
-            if not isinstance(lst, list):
-                return (1)
-            if not lst:
-                return ()
-            shape = []
-            sub_list = lst
-            while isinstance(sub_list, list):
-                shape.append(len(sub_list))
-                if not sub_list:  # If any dimension has 0 elements, stop here
-                    break
-                sub_list = sub_list[0]
-
-            shape = tuple(shape)
-            return shape
         
-        self.shape = get_shape(self.data)
+        shape = []
+        d = self.data
+        while isinstance(d, list):
+            shape.append(len(d))
+            if d:
+                d = d[0]
+            else:
+                break
+
+        self.shape = tuple(shape)
 
     def _set_value(self, value):
         assert isinstance(value, (list, int, float)), 'tensor input cannot be of not number'
         if not isinstance(value, list):
             value = [value]
-        
+
+        # TODO: Check whether every dimension of the data is fine, we cannot do something like [[1, 2], [3, 4, 5]]
         self.data = value
         self._set_shape()
 
     def __getitem__(self, key):
-        if isinstance(key, tuple) and len(key) > len(self.shape):
-            raise ValueError(f'cannot slice ({len(key)}) more than shape of the Tensor ({len(self.shape)})')
-        
-        if isinstance(key, (slice, int)) and len(self.shape) < 1:
-            raise ValueError(f'cannot slice (1) more than shape of the Tensor (< 1)')
-        
-        def _handle_slice_index(key: slice, len_data: int) -> slice:
-            step = 1 if not key.step else key.step
+        if not isinstance(key, tuple):
+            key = (key,)
 
-            start = key.start
-            if not start:
-                start = 0 if step > 0 else len_data - 1
+        def recursive_slice(data, indices):
+            if not isinstance(data, list):
+                return data
 
-            stop = key.stop
-            if not stop:
-                stop = len_data + 1 if step > 0 else -len_data - 1
+            if not indices:
+                return data
 
-            if start < 0:
-                while start > 0:
-                    start = len_data + start
+            current_index = indices[0]
+            if isinstance(current_index, slice):
+                return [recursive_slice(item, indices[1:]) for item in data[current_index]]
+            elif isinstance(current_index, int):
+                return recursive_slice(data[current_index], indices[1:])
+            else:
+                raise TypeError("Invalid index type")
 
-            if stop < 0:
-                while stop > 0:
-                    stop = len_data + stop
-
-            return slice(start, stop, step)
-        
-        def __handle_integer_index(key: int, len_data: int) -> int:
-            if key > len_data:
-                # raise IndexError(f'index 3 is out of bounds for axis 0 with size 3')  # TODO: Give axis information
-                raise IndexError(f'index {key} is out of bounds for axis with size {len_data}')
-            
-            if key < 0:
-                key = key + len_data
-            
-            return key
-
-        val_return = None
-        if isinstance(key, slice):
-            key = _handle_slice_index(key, len(self.data))
-            val_return = self.data[key.start:key.stop:key.step]
-        elif isinstance(key, tuple):
-            val_return = self.data
-            low_rank = False
-            for dim in key:
-                print(f'Current val: {val_return}')
-                print(f'low rank: {low_rank}')
-                if isinstance(dim, slice):
-                    # listeyken slice ile icinde liste donduysen rank dustu
-                    # listeyken slice ile liste donmediysen rank sabit
-
-                    # su an rank dusmuyor.........
-                    if low_rank:
-                        while len(val_return) == 1:
-                            val_return = val_return[0]
-                    dim = _handle_slice_index(dim, len(val_return))
-                    print(f"Inner slice is: {dim}")
-                    val_return = [item[dim.start:dim.stop:dim.step] for item in val_return] if low_rank else val_return[dim.start:dim.stop:dim.step]
-                    low_rank = isinstance(val_return[0], list)
-
-                elif isinstance(dim, int):
-                    print(f"Inner int dim is: {dim}")
-                    dim = __handle_integer_index(dim, len(val_return))
-                    val_return = [item[dim] for item in val_return] if low_rank else val_return[dim]
-                else:
-                    raise ValueError(f'slicing with {type(dim)} is not supported')
-        elif isinstance(key, int):
-            key = __handle_integer_index(key, len(self.data))
-            val_return = self.data[key]
-        else:
-            raise ValueError(f'slicing with {type(key)} is not supported')
-        
-        if val_return is None:
-            raise ValueError(f'return value of none!')
-        
-        return Tensor(val_return)
+        result_data = recursive_slice(self.data, key)
+          
+        return Tensor(result_data)
 
     def __len__(self):
-        return len(self.data)
+        return self.shape[0]
 
     def __add__(self, other):
         assert isinstance(other, Tensor), 'summation is being support only with another Tensor'
@@ -148,10 +82,7 @@ class Tensor:
             else:
                 return [zero_fill(shape[1:]) for _ in range(shape[0])]
         
-        value = zero_fill(shape)
-        tensor = Tensor()
-        tensor._set_value(value)
-
+        tensor = Tensor(zero_fill(shape))
         return tensor
 
 
